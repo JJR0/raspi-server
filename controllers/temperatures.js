5e0b3fc0dfc817cbded6690bdaf0b0d38fb80a17
JRR0 <joonas.rantanen91@gmail.com>
2018-09-15 16:19:22 +0300

    added new POST paths

--- controllers/temperatures.js
+++ controllers/temperatures.js
@@ -33,12 +33,6 @@ temperaturesRouter.get('/:date', async (request, response) => {
     if (typeof temp[0] === 'undefined') {
       response.status(404).json({ error: 'temperature data not found on that date' })
     } else {
-      // const temperatures = temp[0].temperatures
-      // const id = temp[0]._id
-
-      // console.log('löydetty temp: ', temperatures)
-      // console.log('löydetty temp: ', id)
-
       response.json(temp)
     }
   } catch (exception) {
@@ -49,7 +43,6 @@ temperaturesRouter.get('/:date', async (request, response) => {
 
 temperaturesRouter.post('/', (request, response) => {
   const updateFreq = request.body
-  // console.log(updateFreq)
 
   if (Object.keys(updateFreq).length === 0 && updateFreq.constructor === Object) {
     response.status(400).json({ error: 'content missing' })
@@ -62,6 +55,11 @@ temperaturesRouter.post('/', (request, response) => {
 temperaturesRouter.post('/olohuone', (request, response) => {
   const body = request.body
   response.json(body['temp'])
+})
+
+temperaturesRouter.post('/ulko', (request, response) => {
+  const body = request.body
+  response.json(body['ulko'])
 })
 
 module.exports = { temperaturesRouter, freq }
